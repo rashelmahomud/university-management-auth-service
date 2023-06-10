66a3498879475299fc7930df84c4a26c5bb3ceec
MCTC <rashelmahmudraj1998@gmail.com>
2023-06-10 09:53:01 +0600

    [Done]

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -1,5 +1,14 @@
 import { IGenericErrorMessage } from './error';
 
+export type IGenericResponce<T> = {
+  meta: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data: T;
+};
+
 export type IGenericErrorResponce = {
   statusCode: number;
   message: string;
