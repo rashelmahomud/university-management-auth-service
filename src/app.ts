c089c2f44baba0c4da1b733286a7605a291250aa
MCTC <rashelmahmudraj1998@gmail.com>
2023-06-24 16:53:09 +0600

    added refreshtoken

--- src/app.ts
+++ src/app.ts
@@ -10,32 +10,21 @@ import cors from 'cors';
 import globallErrorHandelars from './app/moddlewars/globallErrorHandelars';
 import routers from './app/routs';
 import httpStatus from 'http-status';
-
+import cookieParser from 'cookie-parser';
 const app: Application = express();
 
 // const port = 5000
 
 app.use(cors());
+app.use(cookieParser());
 
 // perser
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-// applicaitons
-
-// app.use('/api/v1/user', UserRoutes);
-// app.use('/api/v1/academec-semester', AcademicSemesterRoute);
-
 app.use('/api/v1/', routers);
 
-// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-//   throw new Error('testing logger...')
-// })
-
-//global error...
 app.use(globallErrorHandelars);
-
-// api not found || kono kisu kaj na korle sitear ja dakabe>
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
